44205f1c2245f55c7924f9f5ee72d95cab98a375
harshHcl1017 <harsh-kuma@hcltech.com>
2026-06-11 00:13:36 +0530

    Added instruction in  readme.md

--- src/main/java/com/hcl/com/simplenms/repository/RouterRepository.java
+++ src/main/java/com/hcl/com/simplenms/repository/RouterRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RouterRepository extends JpaRepository<Router, String> {
+public interface RouterRepository extends JpaRepository<Router, Long> {
 
     List<Router> findByRouterId(String s);
 
